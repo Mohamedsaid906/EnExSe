e99032f656addde2e06c63dcb1e66551dc34e4b5
Mohamed said taleb <MohamedSaid.Taleb@hydatis.com>
2022-11-26 14:08:44 +0100

    create project

---
+++ Micro-Service-Authentification/src/main/java/EnExSe/MicroServiceAuthentification/MicroServiceAuthentificationApplication.java
@@ -0,0 +1,13 @@
+package EnExSe.MicroServiceAuthentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceAuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceAuthentificationApplication.class, args);
+	}
+
+}
---
+++ Micro-Service-Authentification/src/test/java/EnExSe/MicroServiceAuthentification/MicroServiceAuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package EnExSe.MicroServiceAuthentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceAuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
